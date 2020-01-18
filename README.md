cb3ab6296c4534ef9cb9b0f6092f493268e82f26
Philipp Scheer <scheer28philipp@gmail.com>
2020-01-18 19:31:36 +0100

    verdant start

---
+++ README.md
@@ -0,0 +1,17 @@
+# Verdant
+
+Verdant is a cryptocurrency aimed at accessibility and adoption
+
+
+## Coin fundamentals
+
+```
+   Total Supply : 50 Million
+         Ticker : VRDT
+ Emission speed : 24 (50% released after 17 years)
+     Block time : 45 seconds
+	   P2P Port : 9012
+	   RPC Port : 9013
+	   Min. Fee : 0.05
+ Address Prefix : Vrdt
+```
---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x4e, 0x4b, 0xe7, 0x15, 0x92, 0x9c, 0x0d, 0x9e, 0x2f, 0xcd, 0x31, 0x4f, 0xaf, 0xec, 0xab, 0x91 } };
+}
