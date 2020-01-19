0b071e9e9ae6deb8c99e4d5b2deb0c9f6267e1d5
Philipp Scheer <scheer28philipp@gmail.com>
2020-01-19 18:28:08 +0100

    fix errors and add genesis tx

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -67,7 +67,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "verdant";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[] = "013c01ff000102029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101172e6533ecbbf7c31da94ef0f7557a417ac267cbf1061e115815b3ceb7a25739";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
